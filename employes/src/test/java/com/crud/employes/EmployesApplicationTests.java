195b723b5a6bda6ab6bae9bec6fe89e9bb4b8801
caor87 <caoralejayese@gmail.com>
2023-03-24 13:20:30 -0500

    add project funtional

--- employes/src/test/java/com/crud/employes/EmployesApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.crud.employes;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EmployesApplicationTests {
-
-}
