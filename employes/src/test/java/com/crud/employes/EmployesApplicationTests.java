ea973deec5ae4f21b990f7ca91c2cc1b60e4b999
caor87 <caoralejayese@gmail.com>
2023-03-24 11:47:01 -0500

    Add project employes

---
+++ employes/src/test/java/com/crud/employes/EmployesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.employes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
